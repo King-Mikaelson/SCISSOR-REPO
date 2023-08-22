99c9e2a180a1e00109eb78c6cb765cfc40c42955
King-Mikaelson <anazodomichael27@gmail.com>
2023-08-22 11:56:13 +0100

    added gilroy font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import localFont from "next/font/local";
+
+const gilroy = localFont({
+  src:[
+    {
+      path:"../../public/font/Gilroy/Gilroy-Medium.ttf",
+      weight:'500'
+    },
+    {
+      path:"../../public/font/Gilroy/Gilroy-Semibold.ttf",
+      weight:'600'
+    },
+    {
+      path:"../../public/font/Gilroy/Gilroy-Bold.ttf",
+      weight:'700'
+    },
+  ],
+  variable:"--font-gilroy"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <main className={`${gilroy.variable} font-sans`}>
+  <Component {...pageProps} />
+  </main>
+  )
 }
+
+{/* <main className={`${nunito.variable} ${publicSans.variable} ${inter.variable} ${nunitoSans.variable}  font-sans`}>
+<Component {...pageProps} />
+</main> */}
